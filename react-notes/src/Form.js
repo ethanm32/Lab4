44147e37179112d8e0c178ea61002371d9f39634
Ethan Moran <c19305451@mytudublin.ie>
2022-12-17 01:16:06 +0000

    ensuring a simple note is created

--- react-notes/src/Form.js
+++ react-notes/src/Form.js
@@ -1,13 +1,31 @@
+import { useRef, useState } from "react";
+
 function Form() {
+    let counter = 0;
+    const textnote = useRef(null);
+    const [notes, setNotes] = useState(['','']);
     function Submit(e) {
-      e.preventDefault();
-      console.log("Create Note");
+        e.preventDefault();
+        setNotes(current => [...current, textnote.current.value])
+
     }
   
     return (
       <form onSubmit={Submit}>
-        <textarea></textarea>
+        <input 
+        ref={textnote} 
+        type="text" 
+        id="text-note"/>
         <button type='submit'>Create Note</button>
+        <div>
+        {notes.map((element,index) => {
+            return (
+                <div key={index}>
+                    <p>{element}</p>
+                </div>
+            );
+        })}
+        </div>
       </form>
     );
   
