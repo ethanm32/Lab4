011c933069d375999ac1b938e275798e8d810bd0
Ethan Moran <c19305451@mytudublin.ie>
2022-12-17 22:13:17 +0000

    colour change list

--- react-notes/src/Form.js
+++ react-notes/src/Form.js
@@ -1,17 +1,36 @@
 import { useRef, useState } from "react";
-
 function Form() {
     let counter = 0;
     const textnote = useRef(null);
+    const colours = useRef(null);
+    const[selectColour, setSelectColour] = useState('');
     const [notes, setNotes] = useState(['','']);
+  
     function Submit(e) {
         e.preventDefault();
         setNotes(current => [...current, textnote.current.value])
-
+        console.log(colours.current.value)
+        
+        
     }
-  
+
+    function handleChange(e) {
+      setSelectColour(e.target.value);
+    }
+    
     return (
       <form onSubmit={Submit}>
+        <select
+          ref={colours}
+          id="colours"
+          value={selectColour}
+          onChange={handleChange}
+          >
+            <option value={'blue'}>Blue</option>
+            <option value={'red'}>Red</option>
+            <option value={'yellow'}>Yellow</option>
+            <option value={'Green'}>Green</option>
+        </select>
         <input 
         ref={textnote} 
         type="text" 
