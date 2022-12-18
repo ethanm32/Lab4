5e8e4576eabf1ba6b23d01ef9126b8450b01ce1c
Ethan Moran <c19305451@mytudublin.ie>
2022-12-18 01:43:29 +0000

    edit and delete almost finished

--- react-notes/src/App.js
+++ react-notes/src/App.js
@@ -18,20 +18,3 @@ function App() {
 
 export default App;
 
-/* 
-<h1> Notes APP</h1>
-  <hr>
-  <h4>Add a note below</h4>
-  <select name="colours" id="colour">
-      <option value="blue">Blue</option>
-      <option value="green">Green</option>
-      <option value="red">Red</option>
-      <option value="yellow">Yellow</option>
-  </select>
-  
-  
-  <textarea id="text-note"></textarea>
-  <button onclick="createNote()">Add note</button>
-  <br>
-  <br>
-  <script src="script-notes.js"></script> */
--- react-notes/src/Form.js
+++ react-notes/src/Form.js
@@ -1,22 +1,36 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect} from "react";
+import React from "react";
+import { ReactDOM } from "react";
+
 function Form() {
     let counter = 0;
     const textnote = useRef(null);
     const colours = useRef(null);
     const[selectColour, setSelectColour] = useState('');
+    const[selectBg, setSelectBg] = useState('')
     const [notes, setNotes] = useState(['','']);
-  
+    const [buttons, setButton] = useState([]);
     function Submit(e) {
+        counter++;
         e.preventDefault();
         setNotes(current => [...current, textnote.current.value])
         console.log(colours.current.value)
+        const colour = colours.current.value;
+        setSelectBg(colour)
+        
         
         
     }
 
     function handleChange(e) {
       setSelectColour(e.target.value);
+
     }
+
+    const hello = (index) => {
+      setNotes(notes.filter((o, i) => index !== i));
+  };
+
     
     return (
       <form onSubmit={Submit}>
@@ -37,18 +51,24 @@ function Form() {
         id="text-note"/>
         <button type='submit'>Create Note</button>
         <div>
+          
         {notes.map((element,index) => {
             return (
-                <div key={index}>
-                    <p>{element}</p>
+                <div contentEditable={true} key={index}  id='note' style={{ backgroundColor: selectBg}}>
+                  {element}
+                  <button onClick={() => hello(index)}>Delete</button>  
+                    
                 </div>
+                
             );
+            
         })}
+        
         </div>
       </form>
     );
-  
+    
   }
   
-  
+ 
   export default Form;
